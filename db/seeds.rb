cc5da5b6d4c4f0e9b64394fb3177e86a1cc8b9d6
Daniel Kipp <daniel.kipp@gmail.com>
2023-01-26 20:11:01 +0100

    basic functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Super place',
+  address: 'Rigaer Str. 70, 12345 Berlin',
+  description: 'You will love itYou will love itYou will love itYou will love itYou will love itYou will love itYou will love itYou will love itYou will love it',
+  price_per_night: 300,
+  number_of_guests: 5
+)
+Flat.create!(
+  name: 'Dark place',
+  address: 'Rosenbecker Str. 28, 12689 Berlin',
+  description: 'A trash place',
+  price_per_night: 5,
+  number_of_guests: 1
+)
